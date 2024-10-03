01cb38fceb26c3ba1c816c0c9ce20a3b71f7de36
SAHIL SINGH <sahilsingh18012002@gmail.com>
2024-10-03 16:17:01 +0530

    Add files via upload

---
+++ alphabetCheck.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class alphabetCheck 
+{
+    public static void main(String args[]) throws IOException
+    {
+        char m;
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
+        System.out.print("Enter any alphabet:");
+        m = (char) bf.read();
+        if(m >= 97 && m <= 123)
+        {
+            System.out.println("Lower Case");
+        }
+        else if(m >= 65 && m <= 96)
+        {
+            System.out.println("Upper Case");
+        }
+        else if(m >= 48 && m <= 57)
+        {
+            System.out.println("Digit");
+        }
+    }
+}
