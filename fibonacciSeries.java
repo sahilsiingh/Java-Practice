8e3bc160567f0efd25d78d38e05c155935ad70ee
SAHIL SINGH <sahilsingh18012002@gmail.com>
2024-10-04 11:37:14 +0530

    Add files via upload

---
+++ fibonacciSeries.java
@@ -0,0 +1,22 @@
+public class fibonacciSeries {
+
+    public static void printfib(int a , int b , int   n){
+
+        if(n == 0){
+            return;
+        }
+        int c = a + b;
+        System.out.println(c);
+        printfib(b, c, n-1);
+
+    }
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        System.out.println(a);
+        System.out.println(b);
+        int n = 10;
+
+        printfib(a, b, n-2);
+    }
+}
