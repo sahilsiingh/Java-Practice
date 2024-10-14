5101588bc0168bddcc30aaad624e9bd2f3261f8f
SAHIL SINGH <sahilsingh18012002@gmail.com>
2024-10-14 17:24:00 +0530

    Add files via upload

---
+++ SwapTwoNo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class SwapTwoNo{
+    public static void main(String[] args){
+        int x , y , temp;
+        Scanner s = new Scanner (System.in);
+        System.out.print("Enter first number:");
+        x = s.nextInt();
+        System.out.print("Enter second number:");
+        y = s.nextInt();
+
+        temp = x;
+        x= y;
+        y = temp;
+
+        System.out.println("After swapping");
+        System.out.println("First no:" +x);
+        System.out.println("Second no:"+y);
+
+        s.close();
+    }
+}
