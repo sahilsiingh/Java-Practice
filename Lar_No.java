fb7ad571e0bbe427ee2989310f2fc4b332154534
SAHIL SINGH <sahilsingh18012002@gmail.com>
2024-11-11 22:30:29 +0530

    Update Lar_No.java

--- Lar_No.java
+++ Lar_No.java
@@ -5,7 +5,7 @@ public class Lar_No
     {
         int x , y ,z ;
         Scanner s = new Scanner (System.in);
-        System.out.print("Enter the 1st no.");
+        System.out.print("Enter the 1st no."); 
         x = s.nextInt();
         System.out.print("Enter the 2nd no.");
         y = s.nextInt();
