90e606b6ee9a4860237afe5d79d37a91a048fc7c
SAHIL SINGH <sahilsingh18012002@gmail.com>
2024-11-12 22:21:39 +0530

    Update Postive_Negative.java

--- Postive_Negative.java
+++ Postive_Negative.java
@@ -3,7 +3,7 @@ public class Postive_Negative
 {
     public static void main(String[] args) 
     {
-        int n;
+        int n; 
         Scanner s = new Scanner(System.in);
         System.out.print("Enter the number you want to check:");
         n = s.nextInt();
