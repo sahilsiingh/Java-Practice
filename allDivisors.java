ee8b543ac5ad10b380bb35bae10530e78325c5ff
Sahil Singh <sahilsingh18012002@gmail.com>
2024-12-30 22:28:17 +0530

    Update allDivisors.java

--- allDivisors.java
+++ allDivisors.java
@@ -3,7 +3,7 @@ public class allDivisors {
 
     public static void main(String[] args) {
         int div = 0;
-        int n = 64;
+        int n = 64; 
         for (int i = 1; i <= n; i++) {
             if(n % i == 0){
                 System.out.print(i + " ");
